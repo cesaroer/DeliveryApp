4fe3500b5672e6df05238acb52d2312666a82348
cesaroer <cesarvarcard@gmail.com>
2023-05-20 00:15:50 -0600

    Added register design and callback to login

--- app/src/main/java/com/cezsmartz/deliveryapp/activities/RegisterActivity.kt
+++ app/src/main/java/com/cezsmartz/deliveryapp/activities/RegisterActivity.kt
@@ -1,12 +1,26 @@
 package com.cezsmartz.deliveryapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.cezsmartz.deliveryapp.R
 
 class RegisterActivity : AppCompatActivity() {
+    var imageViewGoToLogin: ImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        imageViewGoToLogin = findViewById(R.id.imageView_go_to_login)
+        imageViewGoToLogin?.setOnClickListener {
+            goToLogin()
+        }
+    }
+
+    private fun goToLogin() {
+        val backIntent = Intent(this, MainActivity::class.java)
+        startActivity(backIntent)
     }
 }
